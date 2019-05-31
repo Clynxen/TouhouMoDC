307268887ebe67e1b11fe4cdd7ff170ec5822b06
Clynxen <novalrx8@gmail.com>
2019-05-31 23:11:44 +0700

    code.5

--- main.lua
+++ main.lua
@@ -7,10 +7,19 @@ function love.load()
   -----------
   ---insert table (currentX, currentY, hp, atk, hit, spd, eva)
   table.insert(playerList, createPlayer(200, 200, 30, 5, 5, 5, 5))
+  table.insert(playerList, createPlayer(200, 200, 30, 5, 5, 5, 5))
   -----
 
+  ---Background board
+  boardX = love.graphics.getWidth() / 4
+  boardY = love.graphics.getHeight() / 6
+  boardSize = love.graphics.getWidth() / 2 --x&y
+  ----------
 
 end
+------------------
+
+
 
 
 --Call functions for every delta time (computer time)
@@ -18,10 +27,10 @@ function love.update(dt)
 
 
 
-
-
-
 end
+---------
+
+
 
 --Call functions to DRAW for every delta time
 function love.draw()
@@ -30,10 +39,19 @@ function love.draw()
   love.graphics.clear(0.5, 0.5, 1)
   -------------------
 
-  --RGB:0-255 ->0-1
+  --Board drawing
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle("fill", boardX, boardY, boardSize, boardSize)
+  --mode: "fill", "line"
+  ----
+
+
+  --RGB:0-255 ->0-1 debugging
   love.graphics.setColor(0, 0, 0)
   love.graphics.print("MouseX: "..love.mouse.getX(), 0, 0)
   love.graphics.print("MouseY: "..love.mouse.getY(), 0, 15)
+  love.graphics.print("Active Players: "..#playerList, 0, 30)
   -------------------
 
 end
+---------------

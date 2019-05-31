d5c1bc9437b2fddfcea15e37d492d157c7c4530e
Clynxen <novalrx8@gmail.com>
2019-05-31 22:51:31 +0700

    code.3

--- constructors.lua
+++ constructors.lua
@@ -1,1 +1,12 @@
 
+---function for creating players
+function createPlayer(currentX, currentY, hp, atk, hit, spd, eva)
+  return { currentX = currentX,
+          currentY = currentY,
+          hp = hp,
+          atk = atk,
+          hit = hit,
+          spd = spd,
+          eva = eva }
+end
+-----------
--- main.lua
+++ main.lua
@@ -1,18 +1,19 @@
 require("constructors")
 
+---Load all variables or functions before all things
 function love.load()
-
-
-
-xxx=69
-xxy=96
-
-
-
+  ---Table list all players
+  playerList = {}
+  -----------
+  ---insert table (currentX, currentY, hp, atk, hit, spd, eva)
+  table.insert(playerList, createPlayer(200, 200, 30, 5, 5, 5, 5))
+  -----
 
 
 end
 
+
+--Call functions for every delta time (computer time)
 function love.update(dt)
 
 
@@ -22,11 +23,19 @@ function love.update(dt)
 
 end
 
+--Call functions to DRAW for every delta time
 function love.draw()
 
+--background color of game
+love.graphics.clear(0.5, 0.5, 1)
+--
 
-
-
+-- RGB:0-255 ->0-1
+-- love.graphics.setColor(red, green, blue, alpha)
+-- love.graphics.print(text, x, y, r, sx, sy, ox, oy, kx, ky)
+--
+-- love.graphics.setColor(red, green, blue, alpha)
+-- love.graphics.print(text, x, y, r, sx, sy, ox, oy, kx, ky)
 
 
 end

3855613b21700e6fe4c8f2f18857901957ed3f15
Clynxen <novalrx8@gmail.com>
2019-05-31 22:57:02 +0700

    code.4

--- main.lua
+++ main.lua
@@ -26,16 +26,14 @@ end
 --Call functions to DRAW for every delta time
 function love.draw()
 
---background color of game
-love.graphics.clear(0.5, 0.5, 1)
---
+  --background color of game
+  love.graphics.clear(0.5, 0.5, 1)
+  -------------------
 
--- RGB:0-255 ->0-1
--- love.graphics.setColor(red, green, blue, alpha)
--- love.graphics.print(text, x, y, r, sx, sy, ox, oy, kx, ky)
---
--- love.graphics.setColor(red, green, blue, alpha)
--- love.graphics.print(text, x, y, r, sx, sy, ox, oy, kx, ky)
-
+  --RGB:0-255 ->0-1
+  love.graphics.setColor(0, 0, 0)
+  love.graphics.print("MouseX: "..love.mouse.getX(), 0, 0)
+  love.graphics.print("MouseY: "..love.mouse.getY(), 0, 15)
+  -------------------
 
 end

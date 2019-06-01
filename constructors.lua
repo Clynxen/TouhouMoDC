5be0c302cfd549b71e3bdc534bd40ed66a978e28
Clynxen <novalrx8@gmail.com>
2019-06-01 09:18:22 +0700

    code.7 - Board grids drawn

--- constructors.lua
+++ constructors.lua
@@ -1,3 +1,25 @@
+--formula distance between points
+function pointDistance(x1, y1, x2, y2)
+  return math.sqrt((y2-y1)^2 + (x2-x1)^2)
+end
+
+function manhattanDistance(fromX, toX, fromY, toY)
+
+  local mDdistanceX = fromX + toX
+  local mDdistanceY = fromY + toY
+
+  if mDdistanceX < 0 then
+    mDdistanceX = mDdistanceX * -1
+  end
+
+  if mDdistanceY < 0 then
+    mDdistanceY = mDdistanceY * -1
+  end
+
+  return (mDdistanceX + mDdistanceY)
+end
+
+
 
 ---function for creating players
 function createPlayer(currentX, currentY, hp, atk, hit, spd, eva)
@@ -10,3 +32,19 @@ function createPlayer(currentX, currentY, hp, atk, hit, spd, eva)
           eva = eva }
 end
 -----------
+
+-----function for grid making
+function newGrid(x, y)
+  return {  x = x,
+            y = y }
+end
+------------
+
+----draw function for making actual grids
+function drawGrid()
+  for i, grid in ipairs(gridList) do
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("line", gridList[i].x, gridList[i].y, nativeGrid, nativeGrid)
+  end
+end
+------
--- main.lua
+++ main.lua
@@ -2,19 +2,35 @@ require("constructors")
 
 ---Load all variables or functions before all things
 function love.load()
+
   ---Table list all players
   playerList = {}
   -----------
+
+  --Table list all the grids
+  gridList = {}
+  -----------
+
+  ---Background board & grid
+  boardX = love.graphics.getWidth() / 4 --1280/4 = 320
+  boardY = love.graphics.getHeight() / 6 --960/6 = 160
+  boardSize = love.graphics.getWidth() / 2 --x&y --1280/2 = 640
+  nativeGrid =  boardX / 10
+  ----------
+
   ---insert table (currentX, currentY, hp, atk, hit, spd, eva) --insert player
   table.insert(playerList, createPlayer(200, 200, 30, 5, 5, 5, 5))
   table.insert(playerList, createPlayer(200, 200, 30, 5, 5, 5, 5))
   -----
 
-  ---Background board
-  boardX = love.graphics.getWidth() / 4
-  boardY = love.graphics.getHeight() / 6
-  boardSize = love.graphics.getWidth() / 2 --x&y
-  ----------
+  ---table insert for making grids, it loops through x and y, 20 x 20 = 400 grids
+  for i = 0, 19, 1 do
+    for z = 0, 19, 1 do
+      table.insert(gridList, newGrid(boardX + nativeGrid * z, boardY + nativeGrid * i))
+    end
+  end
+  -----------
+
 
 end
 ------------------
@@ -45,12 +61,21 @@ function love.draw()
   --mode: "fill", "line"
   ----
 
+  --Grid Drawing
+  drawGrid()
+  --
+
 
   --RGB:0-255 ->0-1 debugging
   love.graphics.setColor(0, 0, 0)
   love.graphics.print("MouseX: "..love.mouse.getX(), 0, 0)
   love.graphics.print("MouseY: "..love.mouse.getY(), 0, 15)
   love.graphics.print("Active Players: "..#playerList, 0, 30)
+  love.graphics.print("Active Grids: "..#gridList, 0, 45)
+  love.graphics.print("Grid 20 x:"..gridList[20].x, 0, 60)
+  love.graphics.print("Grid 20 y:"..gridList[20].y, 0, 75)
+  love.graphics.print("Grid 21 x:"..gridList[21].x, 0, 90)
+  love.graphics.print("Grid 21 y:"..gridList[21].y, 0, 105)
   -------------------
 
 end

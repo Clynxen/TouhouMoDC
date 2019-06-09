ca1addd903f26f2997368b3e25e5d305bbcffbe8
Clynxen <novalrx8@gmail.com>
2019-06-09 13:44:18 +0700

    code.8 - basics layout

--- constructors.lua
+++ constructors.lua
@@ -2,11 +2,13 @@
 function pointDistance(x1, y1, x2, y2)
   return math.sqrt((y2-y1)^2 + (x2-x1)^2)
 end
+-----------
 
+----function for manhattanDistance which makes a diamond like range
 function manhattanDistance(fromX, toX, fromY, toY)
 
-  local mDdistanceX = fromX + toX
-  local mDdistanceY = fromY + toY
+  local mDdistanceX = fromX - toX
+  local mDdistanceY = fromY - toY
 
   if mDdistanceX < 0 then
     mDdistanceX = mDdistanceX * -1
@@ -18,18 +20,20 @@ function manhattanDistance(fromX, toX, fromY, toY)
 
   return (mDdistanceX + mDdistanceY)
 end
-
+---------
 
 
 ---function for creating players
-function createPlayer(currentX, currentY, hp, atk, hit, spd, eva)
-  return { currentX = currentX,
+function createPlayer(playerNo, currentX, currentY, hp, atk, hit, spd, eva)
+  return { playerNo = playerNo,
+          currentX = currentX,
           currentY = currentY,
           hp = hp,
           atk = atk,
           hit = hit,
           spd = spd,
-          eva = eva }
+          eva = eva,
+          currentSelected = false}
 end
 -----------
 
@@ -40,6 +44,140 @@ function newGrid(x, y)
 end
 ------------
 
+
+---function for the grids if hovered change color
+function hoveredGrid()
+  for i, grid in ipairs(gridList) do
+    if love.mouse.getX() >= gridList[i].x and love.mouse.getX() <= gridList[i].x + nativeGrid
+    and love.mouse.getY() >= gridList[i].y and love.mouse.getY() <= gridList[i].y + nativeGrid then
+      love.graphics.setColor(0.4, 1, 1)
+      love.graphics.rectangle("fill", gridList[i].x, gridList[i].y, nativeGrid, nativeGrid)
+    end
+  end
+end
+---
+
+--function for snapping players to the grids
+function playerGridSnapper()
+  for i = boardX, boardX + boardSize, nativeGrid do
+
+    for j, players in ipairs(playerList) do
+      if playerList[j].currentSelected == true then
+        if love.mouse.getX() >= i and love.mouse.getX() <= i + boardSize then
+          if love.mouse.isDown(1) then
+            playerList[j].currentX = i
+          end
+        end
+      end
+    end
+
+  end
+
+  for i = boardY, boardY + boardSize, nativeGrid do
+
+    for j, players in ipairs(playerList) do
+      if playerList[j].currentSelected == true then
+        if love.mouse.getY() >= i and love.mouse.getY() <= i + boardSize then
+          if love.mouse.isDown(1) then
+            playerList[j].currentY = i
+          end
+        end
+      end
+    end
+
+  end
+end
+-----
+
+--function for player clicked selected mode and not clicked selected mode
+function playerSelectClick()
+  function love.mousepressed(x, y, button, isTouch)
+    if button == 1 then
+
+      for i, players in ipairs(playerList) do
+        if love.mouse.getX() >= playerList[i].currentX and love.mouse.getX() <= playerList[i].currentX + nativeGrid
+        and love.mouse.getY() >= playerList[i].currentY and love.mouse.getY() <= playerList[i].currentY + nativeGrid then
+          playerList[i].currentSelected = true
+        end
+      end
+
+    elseif button == 2 then
+      for i, players in ipairs(playerList) do
+          playerList[i].currentSelected = false
+      end
+    end
+
+  end
+end
+---
+
+--function for player movement
+function playerMoveIfSelected()
+  for i, players in ipairs(playerList) do
+    if playerList[i].currentSelected == true then
+      if love.mouse.isDown(1) then
+        if manhattanDistance(playerList[i].currentX + nativeGrid / 2, love.mouse.getX(), playerList[i].currentY + nativeGrid / 2, love.mouse.getY())
+        <= (playerList[i].spd * nativeGrid) then
+
+          for z, grids in ipairs(gridList) do
+            if manhattanDistance(playerList[i].currentX, gridList[z].x, playerList[i].currentY, gridList[z].y)
+            <= (playerList[i].spd * nativeGrid) then
+
+              playerList[i].currentX = gridList[z].x
+              playerList[i].currentY = gridList[z].y
+
+              playerGridSnapper()
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
+--
+
+--function for the player grids to be only moved inside the grids
+  function playerBoundariesX()
+    for i, players in ipairs(playerList) do
+      if playerList[i].currentX < boardX then
+        playerList[i].currentX = boardX
+      elseif playerList[i].currentX > boardX + boardSize - nativeGrid then
+        playerList[i].currentX = boardX + boardSize - nativeGrid
+      end
+    end
+  end
+
+  function playerBoundariesY()
+    for i, players in ipairs(playerList) do
+      if playerList[i].currentY < boardY then
+        playerList[i].currentY = boardY
+      elseif playerList[i].currentY > boardY + boardSize - nativeGrid then
+        playerList[i].currentY = boardY + boardSize - nativeGrid
+      end
+    end
+  end
+--------
+
+
+
+--function for highlighting movement range
+function highlightPlayerMoveRange()
+  for i, players in ipairs(playerList) do
+    if playerList[i].currentSelected == true then
+
+      for z, grid in ipairs(gridList) do
+        if manhattanDistance(playerList[i].currentX + nativeGrid / 2, gridList[z].x + nativeGrid / 2, playerList[i].currentY + nativeGrid / 2, gridList[z].y + nativeGrid / 2) <= playerList[i].spd * nativeGrid then
+          love.graphics.setColor(0, 0, 0)
+          love.graphics.rectangle("fill", gridList[z].x + nativeGrid / 16, gridList[z].y + nativeGrid / 16, nativeGrid - 4, nativeGrid - 4)
+        end
+      end
+
+    end
+  end
+end
+--
+
 ----draw function for making actual grids
 function drawGrid()
   for i, grid in ipairs(gridList) do
@@ -48,3 +186,12 @@ function drawGrid()
   end
 end
 ------
+
+--draw function for the players
+function drawPlayer()
+  for i, player in ipairs(playerList) do
+    love.graphics.setColor(1, 0.4, 0.4)
+    love.graphics.rectangle("fill", playerList[i].currentX, playerList[i].currentY, nativeGrid, nativeGrid)
+  end
+end
+---
--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+--[[TODO:
+-optimise the goddamn range movement
+]]
+
 require("constructors")
 
 ---Load all variables or functions before all things
@@ -15,12 +19,12 @@ function love.load()
   boardX = love.graphics.getWidth() / 4 --1280/4 = 320
   boardY = love.graphics.getHeight() / 6 --960/6 = 160
   boardSize = love.graphics.getWidth() / 2 --x&y --1280/2 = 640
-  nativeGrid =  boardX / 10
+  nativeGrid =  boardX / 10 --32
   ----------
 
-  ---insert table (currentX, currentY, hp, atk, hit, spd, eva) --insert player
-  table.insert(playerList, createPlayer(200, 200, 30, 5, 5, 5, 5))
-  table.insert(playerList, createPlayer(200, 200, 30, 5, 5, 5, 5))
+  ---insert table (playerNo, currentX, currentY, hp, atk, hit, spd, eva) --insert player
+  table.insert(playerList, createPlayer(1, 450, 200, 30, 5, 5, 5, 5))
+  table.insert(playerList, createPlayer(2, 500, 200, 30, 5, 5, 3, 5))
   -----
 
   ---table insert for making grids, it loops through x and y, 20 x 20 = 400 grids
@@ -41,7 +45,19 @@ end
 --Call functions for every delta time (computer time)
 function love.update(dt)
 
+  --mouse clicked change the player mode between selected and not
+  playerSelectClick()
+  playerMoveIfSelected()
+  -----
 
+  --player grid snapper to the grids
+  --playerGridSnapper()
+  --
+
+  --player limit boundaries inside the boardX
+  playerBoundariesX()
+  playerBoundariesY()
+  --
 
 end
 ---------
@@ -65,8 +81,19 @@ function love.draw()
   drawGrid()
   --
 
+  --hovered grid change colors
+  hoveredGrid()
+  -----
 
-  --RGB:0-255 ->0-1 debugging
+  --highlight player movement range if selected
+  highlightPlayerMoveRange()
+  --
+
+  --drawing actual players
+  drawPlayer()
+  ---
+
+  --RGB:0-255 ->0-1 debugging textes
   love.graphics.setColor(0, 0, 0)
   love.graphics.print("MouseX: "..love.mouse.getX(), 0, 0)
   love.graphics.print("MouseY: "..love.mouse.getY(), 0, 15)
@@ -76,6 +103,10 @@ function love.draw()
   love.graphics.print("Grid 20 y:"..gridList[20].y, 0, 75)
   love.graphics.print("Grid 21 x:"..gridList[21].x, 0, 90)
   love.graphics.print("Grid 21 y:"..gridList[21].y, 0, 105)
+  for i, players in ipairs(playerList) do
+    love.graphics.print("Player "..playerList[i].playerNo.." X:"..playerList[i].currentX, 0, playerList[i].playerNo * 15 + 105)
+    love.graphics.print("Player "..playerList[i].playerNo.." Y:"..playerList[i].currentY, 0, playerList[i].playerNo * 15 + 140)
+  end
   -------------------
 
 end
